38d974b4fc1a873e2ba9d1c3258266ad234ff7f6
kritoma <toma.kristian97@gmail.com>
2022-07-06 14:32:48 +0200

    first commit

---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home page</title>
+</head>
+<body>
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ad cum alias quasi velit delectus quidem ullam et, quod nam eligendi! Alias voluptates, eveniet inventore sed tenetur dolorem beatae aut.</p>
+</body>
+</html>
